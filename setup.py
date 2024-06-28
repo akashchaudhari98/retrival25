612f2d3ca7caf3bb0a9fe085d5a6512044710459
Akash Chaudhari <akashchaudhari726@gmail.com>
2024-06-28 06:56:33 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='retrival25',
-    version='0.1.0',
+    version='0.1',
     packages=find_packages(),
     install_requires=[
         # List your project's dependencies here.
